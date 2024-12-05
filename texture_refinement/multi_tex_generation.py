b220cbb7d87521129519482b6ba1f63941a6b2b3
Grav1tum <362449104@qq.com>
2024-12-05 19:09:15 +0800

    texture update

--- texture_refinement/multi_tex_generation.py
+++ texture_refinement/multi_tex_generation.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from argparse import ArgumentParser
 import torchvision.transforms
+import os
 
 def downsample_and_rotate(image_path, save_path):
 
@@ -12,22 +13,22 @@ def downsample_and_rotate(image_path, save_path):
 
     # crop scale(texture scale control)
     H_crop, W_crop = img.height // 2, img.width // 2
-    crop_op = torchvision.transforms.CenterCrop((W_crop, H_crop))
+    width, height = (W_crop, H_crop)
+    
+    crop_op = torchvision.transforms.CenterCrop((W_crop, H_crop)) 
 
     for i in range(0, 16):
         img_rotated = img_downsampled.rotate(22.5 * i, expand=True)
         img_rotated = crop_op(img_rotated)
-        rotated_images.append(img_rotated)
+        new_img = Image.new('RGB', (2 * width, 2 * height))
+        for i in range(0, 4):
+            x = (i % 2) * width
+            y = (i // 2) * height
+            new_img.paste(img_rotated, (x, y))
+        rotated_images.append(new_img)
     
-    width, height = (W_crop, H_crop)
-    combined_img = Image.new('RGB', (4 * width, 4 * height))
-    
-    for i, img in enumerate(rotated_images):
-        x = (i % 4) * width
-        y = (i // 4) * height
-        combined_img.paste(img, (x, y))
-    
-    combined_img.save(save_path)
+    for i in range(len(rotated_images)):
+        rotated_images[i].save(os.path.join(save_path, str(i) + '.jpg'))
 
 if __name__ == '__main__':
     parser = ArgumentParser()
