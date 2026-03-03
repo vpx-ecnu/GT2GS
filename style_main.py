10bd5b098890202af26d356342ef56504c441353
Lozical <lozical@qq.com>
2026-03-03 14:58:21 +0800

    remove useless files

--- style_main.py
+++ style_main.py
@@ -30,13 +30,33 @@ def main():
             group=datetime.now().strftime("%Y-%m-%d-%H")
         )
     safe_state(config.app.quiet)
-    network_gui.init(config.app.ip, config.app.port)
+    # network_gui.init(config.app.ip, config.app.port)
     torch.autograd.set_detect_anomaly(config.app.detect_anomaly)
     
     trainer = StyleTrainer(config)
+
+    
+    # ##################################################################
+    # torch.cuda.reset_peak_memory_stats()
+    # starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+    # starter.record()
+    # ##################################################################
     trainer.train()
     
-    
+    # ####################################################################
+    # ender.record()
+    # torch.cuda.synchronize()
+    # elapsed_time_s = starter.elapsed_time(ender) / 1000 # Convert to seconds
+    # peak_mem_gb = torch.cuda.max_memory_allocated() / 1024**3  # Convert to GB
+
+    # print(f"峰值显存占用: {peak_mem_gb:.2f} GB")
+    # print(f"GPU 运行时间: {elapsed_time_s:.2f} s")
+
+    # from pathlib import Path
+    # with open(Path(config.style.stylized_model_path) / "metrics.txt", "w") as f:
+    #     f.write(f"运行时间 (s): {elapsed_time_s:.2f}\n")
+    #     f.write(f"峰值显存占用 (GB): {peak_mem_gb:.2f}\n")
+    # ####################################################################
     # trainer.config.model.model_path = trainer.config.style.stylized_model_path
     # if trainer.config.model.source_path.find("tnt") != -1:
     #     from scripts.render_tnt_video import render_video
@@ -45,7 +65,7 @@ def main():
     # from scripts.render_llff_video import render_video
     # render_video(trainer)
     
-    render_viewpoint(trainer)
+    # render_viewpoint(trainer)
 
 
 if __name__ == "__main__":
